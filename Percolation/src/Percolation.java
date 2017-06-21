0d160ecb8a826502502cb655a455d9c00db87544
Sergey Kleymenov <marchhare@yandex-team.ru>
2017-06-21 13:03:03 +0300

    add input args checks
    * check grid size in the constuctor
    * check row and col args in public methods

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -1,29 +1,47 @@
-/**
- * Percolation problem solution.
- *
- * Created by Sergey Kleymenov on 21/06/2017.
- */
+/*
+  Percolation problem solution.
 
+  Created by Sergey Kleymenov on 21/06/2017.
+ */
+import java.lang.IndexOutOfBoundsException;
+import java.lang.IllegalArgumentException;
 
 
 public class Percolation {
+    private final int size;
+
+    private void checkArgs(int row, int col)
+    {
+        if ( row <= 0 || row > this.size) throw new IndexOutOfBoundsException("row index out of bounds");
+        if ( col <= 0 || col > this.size) throw new IndexOutOfBoundsException("col index aout of bound");
+    }
     public Percolation(int n)                // create n-by-n grid, with all sites blocked
     {
+        if ( n <= 0)
+        {
+            throw new IllegalArgumentException("Grid size must be greater then zero");
+        }
+
+        this.size = n;
 
     }
-    public    void open(int row, int col)    // open site (row, col) if it is not open already
+    public void open(int row, int col)    // open site (row, col) if it is not open already
     {
+        checkArgs(row, col);
 
     }
     public boolean isOpen(int row, int col)  // is site (row, col) open?
     {
+        checkArgs(row, col);
         return true;
     }
     public boolean isFull(int row, int col)  // is site (row, col) full?
     {
+        checkArgs(row, col);
+
         return false;
     }
-    public     int numberOfOpenSites()       // number of open sites
+    public int numberOfOpenSites()       // number of open sites
     {
         return 0;
     }
